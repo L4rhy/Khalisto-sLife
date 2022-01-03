2cf70fecfbc2b722274f904bb47bacfdad9cce22
L4rhy <lara.vsteinbach@gmail.com>
2022-01-03 19:37:57 -0300

    funcionando

--- js/jogo.js
+++ js/jogo.js
@@ -1,54 +1,26 @@
+var falas = {}
+var estados = {}
 
-var estado = 0
+    var leitor1 = new XMLHttpRequest();
+
+    leitor1.open("get", "https://khalisto-json.herokuapp.com/falas/", false);
+    leitor1.onload = function(){
+        falas = JSON.parse(this.responseText)
+        console.log(falas)
+    }
+    leitor1.send();
+
+    var leitor2 = new XMLHttpRequest();
+
+    leitor2.open("get", "https://khalisto-json.herokuapp.com/estados/", false);
+    leitor2.onload = function(){
+        estados = JSON.parse(this.responseText)
+        console.log(estados)
+    }
+    leitor2.send();
+
 var i = 0
-
-var estados = {
-    "estados":[{
-        "estado":{
-            "opcoes":[
-                "Posso ir ao parque perto de casa, gosto de ficar lá",
-                "Posso ir ver minha Avó ingrid, ela tem o melhor bolo de cenoura do mundo",
-                "Posso ir ver minha melhor amiga Amanda, eu a conheço desde sempre"
-            ],
-            "estadosFuturos":[1,2,3],
-            "cenarioURL":"imagens/rua.jpg",
-            "personagensURL":["imagens/khalisto.png", "imagens/khalisto.png"],
-            "pesonagemDireitaA":1000
-        }
-        
-    }]
-}
-var falas = {
-    "dialogos":[{
-        "falas":[
-            {
-            "autor":"khalisto",
-            "fala":"Hoje foi um dia difícil, briguei com minha mãe e sai de casa"
-            },{
-            "autor":"khalisto",
-            "fala":"Nós brigamos quase todo dia porque ela quer me levar a igreja"
-            },{
-            "autor":"khalisto",
-            "fala":"Ela não entende que eu enquanto uma pessoa trans não me sinto bem lá"
-            },{
-            "autor":"khalisto",
-            "fala":"Ela não entende que não me sinto acolhida pela religião dela"
-            },{
-            "autor":"khalisto",
-            "fala":"Mas se não fosse isso seria outra coisa"
-            },{
-            "autor":"khalisto",
-            "fala":"Ela está descontando em mim os próprios traumas"
-            },{
-            "autor":"khalisto",
-            "fala":"Preciso decidir aonde ir pelas próximas horas "
-            }
-        ],
-        "numeroDeFalas":6
-    }]
-}
-
-
+var estado = 0 
 
 const container = document.querySelector("#container")
 const fundo = document.querySelector("#fundo")
@@ -102,7 +74,7 @@ imagemFundo.src = estados["estados"][estado]["estado"]["cenarioURL"]
 fundo.style.backgroundImage = (imagemFundo)
 personagemDireita.src = estados["estados"][estado]["estado"]["personagensURL"][0]
 personagemEsquerda.src = estados["estados"][estado]["estado"]["personagensURL"][1]
-personagemDireita.style.opacity = 0
+apareceDireita(i)
 botaoOpcao1.innerHTML = estados["estados"][estado]["estado"]["opcoes"][0]
 botaoOpcao2.innerHTML = estados["estados"][estado]["estado"]["opcoes"][1]
 botaoOpcao3.innerHTML = estados["estados"][estado]["estado"]["opcoes"][2]
@@ -137,8 +109,10 @@ function desaparece(falas, i){
 console.log("DESAPARECE FUNCIONA")
 }
 function apareceDireita(i){
-if(i=estados["estados"][estado]["estado"]["pesonagemDireitaA"]){
+if(i==estados["estados"][estado]["estado"]["pesonagemDireitaA"]){
     personagemDireita.style.opacity = 1
+    }else{
+    personagemDireita.style.opacity = 0
     }
 }
 
